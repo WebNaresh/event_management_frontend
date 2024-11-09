0914aa6db9e39d9a9200fea4df3ee029a476775f
Naresh Bhosale <nareshbhosale73@gmail.com>
2024-11-09 21:20:00 +0530

    enhance TopNav to conditionally render dropdown menu based on authentication token

--- src/utils/TopNav/TopNav.tsx
+++ src/utils/TopNav/TopNav.tsx
@@ -34,32 +34,34 @@ export default function TopNav() {
     <div className="sticky top-0 z-50 w-full border-b bg-background/95 backdrop-blur supports-[backdrop-filter]:bg-background/60">
       <div className="container flex h-14 items-center justify-between">
         <div className="flex items-center gap-2">
-          <DropdownMenu>
-            <DropdownMenuTrigger asChild>
-              <Button variant="ghost" size="icon">
-                <Apps className="h-5 w-5 text-primary" />
-                <span className="sr-only">Open menu</span>
-              </Button>
-            </DropdownMenuTrigger>
-            <DropdownMenuContent align="start">
-              <DropdownMenuItem>
-                <Link to="/qr_code" className="flex items-center gap-2">
-                  <ScanBarcode className="h-5 w-5 text-primary" />
-                  <span className="text-sm font-bold text-primary">
-                    Show QR
-                  </span>
-                </Link>
-              </DropdownMenuItem>
-              <DropdownMenuItem>
-                <Link to={dashboard()} className="flex items-center gap-2">
-                  <ScanBarcode className="h-5 w-5 text-primary" />
-                  <span className="text-sm font-bold text-primary">
-                    Dashboard
-                  </span>
-                </Link>
-              </DropdownMenuItem>
-            </DropdownMenuContent>
-          </DropdownMenu>
+          {token && (
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <Button variant="ghost" size="icon">
+                  <Apps className="h-5 w-5 text-primary" />
+                  <span className="sr-only">Open menu</span>
+                </Button>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent align="start">
+                <DropdownMenuItem>
+                  <Link to="/qr_code" className="flex items-center gap-2">
+                    <ScanBarcode className="h-5 w-5 text-primary" />
+                    <span className="text-sm font-bold text-primary">
+                      Show QR
+                    </span>
+                  </Link>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <Link to={dashboard()} className="flex items-center gap-2">
+                    <ScanBarcode className="h-5 w-5 text-primary" />
+                    <span className="text-sm font-bold text-primary">
+                      Dashboard
+                    </span>
+                  </Link>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          )}
           <h2 className="text-lg font-bold text-primary">ESM</h2>
         </div>
         {token ? (
